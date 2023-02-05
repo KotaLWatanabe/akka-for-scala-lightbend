8a7754756aaaebc93be6197823ab0422dd5c458d
KotaLWatanabe <KotaLWatanabe@users.noreply.github.com>
2023-02-05 17:49:42 +0900

    solve keep actor busy

--- exercises/src/main/scala/com/lightbend/training/coffeehouse/CoffeeHouse.scala
+++ exercises/src/main/scala/com/lightbend/training/coffeehouse/CoffeeHouse.scala
@@ -17,10 +17,16 @@ class CoffeeHouse extends Actor with ActorLogging {
     context.system.settings.config.getDuration("coffee-house.guest.finish-coffee-duration", TimeUnit.MILLISECONDS).millis
 
   println(finishedCoffeeDuration)
+
+  private val prepareCoffeeDuration: FiniteDuration =
+    context.system.settings.config.getDuration("coffee-house.barista.prepare-coffee-duration", TimeUnit.MILLISECONDS).millis
+  private val barista: ActorRef = createBarista()
   private val waiter: ActorRef = createWaiter()
 
+
+  protected def createBarista(): ActorRef = context.actorOf(Barista.props(prepareCoffeeDuration), "barista")
   protected def createGuest(favoriteCoffee: Coffee): ActorRef = context.actorOf(Guest.props(waiter, favoriteCoffee, finishedCoffeeDuration))
-  private def createWaiter(): ActorRef = context.actorOf(Waiter.props, "waiter")
+  private def createWaiter(): ActorRef = context.actorOf(Waiter.props(barista), "waiter")
 
   override def receive: Receive = {
     case CreateGuest(coffee: Coffee) => createGuest(coffee)
